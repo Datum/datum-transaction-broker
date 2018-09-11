0f31d6495772fb84a608d40d9299684c558a3972
Kareem <kareem@datum.org>
2018-09-11 19:45:38 +0800

    Add toHex function to txService.
    - This is an extra step incase of sender sent the actuall values not hex
    values

--- services/TransactionService.js
+++ services/TransactionService.js
@@ -1,31 +1,58 @@
+const uuid = require('uuid/v1');
+const Web3 = require('web3');
 const logger = require('../utils/Logger');
 const Publisher = require('../workers/Publisher');
 const TxWorker = require('../workers/TxWorker');
 const StatusWorker = require('../workers/StatusWorker');
 const config = require('../utils/Config');
-const uuid = require('uuid/v1');
-
 /**
  * Transaction service
  */
-class TransactionService{
-    constructor(){
-        this.dispatcher = new Publisher([config.queues.transactions],'transaction_dispatcher');
-        this.txWorkers ={};
-        this.statusWorkers={};
+class TransactionService {
+
+  constructor() {
+    this.dispatcher = new Publisher([config.queues.transactions], 'transaction_dispatcher');
+    this.txWorkers = {};
+    this.statusWorkers = {};
+  }
+
+  start() {
+    logger.info('Transaction service starting...');
+    config.accounts.forEach((account) => {
+      this.txWorkers[account.address] = new TxWorker([config.queues.transactions], [`${account.address}_pending`], account);
+      this.statusWorkers[account.address] = new StatusWorker([`${account.address}_pending`], [config.queues.transactions]);
+    });
+  }
+
+  dispatch(request) {
+    const tmp = this.convertVals(request);
+    tmp.id = uuid();
+    logger.debug(`Dispatching tmp ${tmp.id}`);
+    return this.dispatcher.publish(tmp).then(() => Promise.resolve(tmp.id));
+  }
+
+  /**
+   * convertVals - Convert object values to Hex if not already in Hex format
+   *
+   * @param  {Object} request incoming request Object
+   * @return {Object}  Object with hex values
+   */
+  convertVals(request) {
+    const tmpR = { ...request };
+    const keys = Object.keys(tmpR);
+    for (let i = 0; i < keys.length; i += 1) {
+      tmpR[keys[i]] = this.toHex(tmpR[keys[i]]);
     }
-    start(){
-        logger.info('Transaction service starting...');
-        config.accounts.map(account=>{
-            this.txWorkers[account.address]= new TxWorker([config.queues.transactions],[`${account.address}_pending`],account);
-            this.statusWorkers[account.address] = new StatusWorker([`${account.address}_pending`],[config.queues.transactions]);
-        });
-    }
-    dispatch(request){
-        request.id = uuid();
-        return this.dispatcher.publish(request).then(()=>Promise.resolve(request.id));
-    }
-  
+    return tmpR;
+  }
+
+  /**
+   * toHex - Conver value v to hex if not in Hex
+   */
+  toHex(v) {
+    return Web3.utils.isHex(v) ? v : Web3.utils.toHex(v);
+  }
+
 }
 const transactionService = new TransactionService();
 module.exports = transactionService;
