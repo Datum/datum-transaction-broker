1aaeb30509a92f3514ad21d08f0c2586d8b6075c
Ka Lam <kalam@datum.org>
2018-11-28 18:20:31 +0800

    for debug

--- services/RedisService.js
+++ services/RedisService.js
@@ -1,6 +1,15 @@
 const Redis = require('ioredis');
 const uuid = require('uuid/v1');
 const config = require('./ConfigService');
+const logger = require('../utils/Logger');
+
+function _reconnectOnError(err) {
+    var targetError = 'READONLY';
+    if (err.message.slice(0, targetError.length) === targetError) {
+        // Only reconnect when the error starts with "READONLY"
+        return true; // or `return 1;`
+    }
+}
 
 class RedisService {
 
@@ -8,12 +17,32 @@ class RedisService {
     this.instances = {};
   }
 
+
   async getRedisInstance() {
     let tmpRedis;
     if (this.isProd()) {
       tmpRedis = await new Redis.Cluster([
-        config.redis,
-      ]);
+          config.redis,
+        ],
+        {
+          redisOptions:
+            {
+                reconnectOnError: _reconnectOnError
+            }
+        }
+      )
+    }
+
+      // FIXME this is for temp debug
+
+        tmpRedis.on('connect', function () { logger.info('redis connect'); });
+        tmpRedis.on('ready', function () { logger.info('redis ready'); });
+        tmpRedis.on('error', function (err) { logger.info('redis error', err); });
+        tmpRedis.on('close', function () { logger.info('redis close'); });
+        tmpRedis.on('reconnecting', function () { logger.info('redis reconnecting'); });
+        tmpRedis.on('end', function () { logger.info('redis end'); });
+
+
     } else {
       tmpRedis = await new Redis(config.redis);
     }
