c01582f7cc2afa2b15f18b08e044b97c313d55ca
Kareem <kareem@datum.org>
2019-01-11 13:47:59 +0800

    Refactoring logs to allow transports to be disabled
    - File transports now can be disabled from configurations by removing
    the file transport element in logger section.

--- utils/Logger.js
+++ utils/Logger.js
@@ -3,19 +3,38 @@ const winston = require('winston');
 const config = require('../services/ConfigService');
 require('winston-daily-rotate-file');
 
-/**
- * Adding beautiful logging :)
- */
-const transport = new (winston.transports.DailyRotateFile)({
-  filename: path.join(__dirname, '../', 'log', '%DATE%.log'),
-  datePattern: 'YYYY-MM-DD',
-  zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: '14d',
-  handleExceptions: true,
-  level: 'debug',
-});
+function ftc() {
+  const tmp = {
+    enabled: typeof config.logger !== 'undefined' && typeof config.logger.fileTransport !== 'undefined',
+  };
+  if (tmp.enabled) {
+    const { ftPath } = config.logger.fileTransport.path;
+    tmp.path = typeof ftPath !== 'undefined' && ftPath.length !== 0 ? ftPath : path.join(__dirname, '../', 'log', '%DATE%.log');
+  }
+  return tmp;
+}
+function getTransports() {
+  /**
+   * Adding beautiful logging :)
+   */
+  const transports = [];
+  const fileTransportConf = ftc();
+  if (fileTransportConf.enabled) {
+    transports.push(new (winston.transports.DailyRotateFile)({
+      filename: fileTransportConf.path,
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+      handleExceptions: true,
+      level: 'debug',
+    }));
+  }
+  transports.push(new winston.transports.Console({ level: config.logger.level }));
+  return transports;
+}
 
+const transports = getTransports();
 const loggerFormat = winston.format.printf(info => `${info.timestamp} ${info.level} : ${info.message}`);
 
 const logger = winston.createLogger({
@@ -28,9 +47,7 @@ const logger = winston.createLogger({
     winston.format.colorize(),
     loggerFormat,
   ),
-  transports: [
-    transport, new winston.transports.Console({ level: config.logger.level }),
-  ],
+  transports,
 });
 
 module.exports = logger;
